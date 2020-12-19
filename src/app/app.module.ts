687d19b304dadd3e008f1150f0cd2497851aefae
Amit Mourya <amitm@clickwt.com>
2020-12-19 16:54:54 +0530

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxPhotoswipeModule } from 'ngx-photoswipe';
+import { LightboxAdapter, NgxPhotoswipeModule } from 'ngx-photoswipe';
+
+@Injectable()
+export class CustomLightboxAdapter extends LightboxAdapter {
+  fullscreenEl = false;
+  zoomEl = false;
+  maxSpreadZoom = 1;
+  pinchToClose = false;
+  getDoubleTapZoom = (isMouseClick, item) => {
+    return item.initialZoomLevel;
+  };
+}
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, NgxPhotoswipeModule],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxPhotoswipeModule],
+  providers: [
+    { provide: LightboxAdapter, useClass: CustomLightboxAdapter }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
+
+
